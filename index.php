d1b2d65e57848d6ae8b48ba7d6df2aa9c295c7da
sh0uryu <candido_24@hotmail.com>
2020-08-29 10:02:48 -0300

    firt toggle menu view

--- index.php
+++ index.php
@@ -1,29 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
     <link rel="stylesheet" href="style.css">
     <title>Glass Eye</title>
 </head>
+
 <body>
-	<nav id="menu">
-		<a class="menucol" id="home-menu">Home</a>
-		<a class="menucol" id="portfolio-menu">Portfolio</a>
-		<a class="menucol" id="contact-menu">Contact</a>
-		<a class="menucol" id="about-menu">About</a>
-		
-		<footer>
-		<a href="http://github.com/sh0uryu" target="_blank">Cândido Ferreira &copy 2020</a>
-		</footer>
-	</nav>
-	
-	<div id="content">	
-	
-	<h1>Glass eye</h1>
-	
-	</div>
-	
+    <nav class="navigation">
+        <input type="checkbox" name="">
+        <span class="fas fa-bars fa-2x"></span>
+
+        <ul class="menu">
+            <li><a href="#">home</a></li>
+            <li><a href="#">portfolio</a></li>
+            <li><a href="#">contact</a></li>
+            <li><a href="#">about</a></li>
+        </ul>
+
+        <footer>
+            <a href="http://github.com/sh0uryu" target="_blank">Cândido Ferreira &copy 2020</a>
+        </footer>
+    </nav>
+
+    <div id="content"> 
+        <h1>Glass eye</h1>
+
+    </div>
 
 </body>
+
 </html>
