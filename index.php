f39a36d7e0b73ec18966903e66bcef47a381de23
sh0uryu <candido_24@hotmail.com>
2020-08-29 12:02:20 -0300

    adjusting navbar css malfunctions

--- index.php
+++ index.php
@@ -6,24 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
     <link rel="stylesheet" href="style.css">
-    <title>Glass Eye</title>
+    <title>Glass eye</title>
 </head>
 
-<body>
+<body>  
     <nav class="navigation">
         <input type="checkbox" name="">
         <span class="fas fa-bars fa-2x"></span>
 
-        <ul class="menu">
-            <li><a href="#">home</a></li>
-            <li><a href="#">portfolio</a></li>
-            <li><a href="#">contact</a></li>
-            <li><a href="#">about</a></li>
-        </ul>
+        <div class="menu">
+            <a class="menu-item" href="#">home</a>
+            <a class="menu-item" href="#">portfolio</a>
+            <a class="menu-item" href="#">contact</a>
+            <a class="menu-item" href="#">about</a>
+        </div>
+        <a href="http://github.com/sh0uryu" target="_blank" id="footer">Cândido Ferreira &copy 2020</a>
 
-        <footer>
-            <a href="http://github.com/sh0uryu" target="_blank">Cândido Ferreira &copy 2020</a>
-        </footer>
+           
     </nav>
 
     <div id="content"> 
